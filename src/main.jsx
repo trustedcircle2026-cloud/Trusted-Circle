3b7cf1da7ddc4e504e2b5275ae0fce0d5ec12aef
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-06 10:04:28 +0530

    Add global animated loading experience

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import GlobalLoading from './GlobalLoading'
 import './styles.css'
 import './site.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalLoading />
     <App />
   </React.StrictMode>
 )
