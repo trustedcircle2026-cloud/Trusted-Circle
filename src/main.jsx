2481edff4003667116cfdae1524709085711fde6
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-08 11:01:51 +0530

    Mount private ERP application

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErpApp from './ErpApp'
 import GlobalLoading from './GlobalLoading'
 import './styles.css'
 import './site.css'
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalLoading />
     <App />
+    <ErpApp />
   </React.StrictMode>
 )
