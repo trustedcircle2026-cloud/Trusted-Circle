20a44191f81fcf7ed8e182ef2de025f17ea41394
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-10 08:36:49 +0530

    Fix ERP route mounting across hash navigation

--- src/ErpApp.jsx
+++ src/ErpApp.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
 import AdminWorkspace from './AdminWorkspace'
 
 export default function ErpApp(){
-  if(!window.location.hash.replace(/^#\/?/,'').startsWith('erp')) return null
-  return <AdminWorkspace />
+  const [isErp,setIsErp]=useState(()=>window.location.hash.replace(/^#\/?/,'').startsWith('erp'))
+  useEffect(()=>{const onHash=()=>setIsErp(window.location.hash.replace(/^#\/?/,'').startsWith('erp'));window.addEventListener('hashchange',onHash);return()=>window.removeEventListener('hashchange',onHash)},[])
+  return isErp?<AdminWorkspace/>:null
 }
