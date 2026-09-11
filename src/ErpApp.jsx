19df3b843b749a9930a80f7477387813808ec966
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-11 08:54:22 +0530

    Switch ERP to corrected admin workspace

--- src/ErpApp.jsx
+++ src/ErpApp.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react'
-import AdminWorkspace from './AdminWorkspace'
-
+import { useEffect,useState } from 'react'
+import AdminWorkspaceV2 from './AdminWorkspaceV2'
 export default function ErpApp(){
-  const [isErp,setIsErp]=useState(()=>window.location.hash.replace(/^#\/?/,'').startsWith('erp'))
-  useEffect(()=>{const onHash=()=>setIsErp(window.location.hash.replace(/^#\/?/,'').startsWith('erp'));window.addEventListener('hashchange',onHash);return()=>window.removeEventListener('hashchange',onHash)},[])
-  return isErp?<AdminWorkspace/>:null
+ const[isErp,setIsErp]=useState(()=>window.location.hash.replace(/^#\/?/,'').startsWith('erp'))
+ useEffect(()=>{const onHash=()=>setIsErp(window.location.hash.replace(/^#\/?/,'').startsWith('erp'));window.addEventListener('hashchange',onHash);return()=>window.removeEventListener('hashchange',onHash)},[])
+ return isErp?<AdminWorkspaceV2/>:null
 }
