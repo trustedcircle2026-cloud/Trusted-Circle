d1f6a63ea265a477a74b1da1e3fed8aef26092f8
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-12 21:52:37 +0530

    feat(admin): enable management KPI dashboard and page summaries

--- src/admin-main.jsx
+++ src/admin-main.jsx
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react'
 import ReactDOM from 'react-dom/client'
 import AdminWorkspaceV2 from './AdminWorkspaceV2'
 import AdminRemoveUser from './AdminRemoveUser'
+import AdminSummaryEnhancer from './AdminSummaryEnhancer'
 import GlobalLoading from './GlobalLoading'
 import './styles.css'
 import './admin-entry.css'
@@ -18,6 +19,7 @@ function AdminApp(){
  const remove=routeName==='remove-user'||routeName==='erp-remove-user'
  return <>
   <GlobalLoading/>
+  {!remove&&<AdminSummaryEnhancer/>}
   {remove
    ? <AdminRemoveUser token={localStorage.getItem('tc_erp_session')||''} onBack={()=>{window.location.hash='';setRouteName('')}}/>
    : <AdminWorkspaceV2/>
