c42341c3e3288579e25c4481566174c7bb326d6b
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-14 12:06:11 +0530

    Enable redesigned production command center

--- src/admin-main.jsx
+++ src/admin-main.jsx
@@ -4,29 +4,16 @@ import AdminWorkspaceV2 from './AdminWorkspaceV2'
 import AdminRemoveUser from './AdminRemoveUser'
 import AdminKpiAllPages from './AdminKpiAllPages'
 import AdminHeaderControls from './AdminHeaderControls'
+import AdminDashboardRedesign from './AdminDashboardRedesign'
 import GlobalLoading from './GlobalLoading'
 import './styles.css'
 import './admin-entry.css'
 
 function route(){return window.location.hash.replace(/^#\/?/,'')}
-
 function AdminApp(){
  const[routeName,setRouteName]=useState(route())
- useEffect(()=>{
-  const onHash=()=>setRouteName(route())
-  window.addEventListener('hashchange',onHash)
-  return()=>window.removeEventListener('hashchange',onHash)
- },[])
+ useEffect(()=>{const onHash=()=>setRouteName(route());window.addEventListener('hashchange',onHash);return()=>window.removeEventListener('hashchange',onHash)},[])
  const remove=routeName==='remove-user'||routeName==='erp-remove-user'
- return <>
-  <GlobalLoading/>
-  {!remove&&<AdminKpiAllPages/>}
-  {!remove&&<AdminHeaderControls/>}
-  {remove
-   ? <AdminRemoveUser token={localStorage.getItem('tc_erp_session')||''} onBack={()=>{window.location.hash='';setRouteName('')}}/>
-   : <AdminWorkspaceV2/>
-  }
- </>
+ return <><GlobalLoading/>{!remove&&<AdminKpiAllPages/>}{!remove&&<AdminHeaderControls/>}{!remove&&<AdminDashboardRedesign/>}{remove?<AdminRemoveUser token={localStorage.getItem('tc_erp_session')||''} onBack={()=>{window.location.hash='';setRouteName('')}}/>:<AdminWorkspaceV2/>}</>
 }
-
 ReactDOM.createRoot(document.getElementById('root')).render(<AdminApp/>)
