278cc5a87eaa298e297dced8ec8c9bee5cd3b34a
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-14 13:57:43 +0530

    Wire Trusted Circle Books into storefront footer

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import './public-ui.css'
 import './erp-overrides.css'
 import './shopping-upgrade.css'
 import './erp-polish.css'
+import './footer-books-link.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
