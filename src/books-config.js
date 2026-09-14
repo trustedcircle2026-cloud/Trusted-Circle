f716bb152a4582f9ba6b5a1e6656fde13ceb092c
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-14 14:03:16 +0530

    Connect Trusted Circle Books to new Apps Script API

--- src/books-config.js
+++ src/books-config.js
@@ -1,5 +1,4 @@
 // Trusted Circle Books backend configuration.
-// Set this to the Web App URL of the new Google Apps Script deployment
-// when the Books Google Sheet/API is ready.
-export const BOOKS_API_URL=''
+// This endpoint belongs to the dedicated Books Google Sheet + Apps Script backend.
+export const BOOKS_API_URL='https://script.google.com/macros/s/AKfycbzy-L9kW1udS-SzxzHUyOQ4nz2mcsGESqZ_ruwI7cKK55_cH8-u9ZJO39-LZ8d-Eg2knQ/exec'
 export const BOOKS_SESSION_KEY='tc_books_session'
