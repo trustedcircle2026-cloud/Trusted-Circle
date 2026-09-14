f99e361b8a53d06c2a2d5f472de6b91d391435fa
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-14 13:57:15 +0530

    Add Trusted Circle Books backend configuration

---
+++ src/books-config.js
@@ -0,0 +1,5 @@
+// Trusted Circle Books backend configuration.
+// Set this to the Web App URL of the new Google Apps Script deployment
+// when the Books Google Sheet/API is ready.
+export const BOOKS_API_URL=''
+export const BOOKS_SESSION_KEY='tc_books_session'
