70da8a511b8833d2312444d435d05c78cadb7b1d
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-18 08:40:10 +0530

    Connect Trusted Circle Cards to deployed Apps Script backend

--- src/cards-config.js
+++ src/cards-config.js
@@ -1,4 +1,4 @@
-// Trusted Circle Cards frontend configuration.
-// Set this to the deployed Google Apps Script Web App /exec URL.
-export const CARDS_API_URL=''
+// Trusted Circle Cards backend configuration.
+// Dedicated Cards Google Sheet + Apps Script Web App.
+export const CARDS_API_URL='https://script.google.com/macros/s/AKfycbwEOF1tVDv3vnENexoxhJnDDCC7kMQVC_FsxSeeIn1XTd-c_ejPU2usTDLrTqg6_Bazzw/exec'
 export const CARDS_SESSION_KEY='tc_cards_session'
