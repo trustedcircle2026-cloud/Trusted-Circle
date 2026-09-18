1126a2052df0d9c7adce35049cc99b7c9d2a749f
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-18 08:02:02 +0530

    Build Trusted Circle Cards backend

---
+++ src/cards-config.js
@@ -0,0 +1,4 @@
+// Trusted Circle Cards frontend configuration.
+// Set this to the deployed Google Apps Script Web App /exec URL.
+export const CARDS_API_URL=''
+export const CARDS_SESSION_KEY='tc_cards_session'
