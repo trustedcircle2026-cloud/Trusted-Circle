fe96045409286f06b58b9520dafd5f79c9d2cdc6
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-24 15:11:44 +0530

    Remove hidden Cards page assets and links

--- src/cards-config.js
+++
@@ -1,4 +0,0 @@
-// Trusted Circle Cards backend configuration.
-// Dedicated Cards Google Sheet + Apps Script Web App.
-export const CARDS_API_URL='https://script.google.com/macros/s/AKfycbwEOF1tVDv3vnENexoxhJnDDCC7kMQVC_FsxSeeIn1XTd-c_ejPU2usTDLrTqg6_Bazzw/exec'
-export const CARDS_SESSION_KEY='tc_cards_session'
