0957593b61b8a9db378d56ffd8c6742fc39e1d63
trustedcircle2026-cloud <trustedcircle2026@gmail.com>
2026-09-24 15:11:11 +0530

    Remove hidden Books and Cards pages

--- src/books-config.js
+++
@@ -1,4 +0,0 @@
-// Trusted Circle Books backend configuration.
-// This endpoint belongs to the dedicated Books Google Sheet + Apps Script backend.
-export const BOOKS_API_URL='https://script.google.com/macros/s/AKfycbzy-L9kW1udS-SzxzHUyOQ4nz2mcsGESqZ_ruwI7cKK55_cH8-u9ZJO39-LZ8d-Eg2knQ/exec'
-export const BOOKS_SESSION_KEY='tc_books_session'
